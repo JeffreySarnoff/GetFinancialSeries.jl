6d8448b1af4c2586d529a83066220aa550f8339e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-06-09 00:04:29 -0400

    Create lookup.jl

--- src/fred/lookup.jl
+++ src/fred/lookup.jl
@@ -1,19 +1,19 @@
 using Base.Dates
 
 const FredKeyTerms = [(:Nominal, :Real, :Spot), 
-                      (:ConstantMaturity, (:Nominal, :Real), (:Day, :Week, :Month)),
-                      (:Forward, (:InstantRate, :TermPremium), (:Day,)),
-                      (:BreakevenInflation, (:InstantRate, :TermPremium), (:Day, :Month)),
-                      (:ZeroCoupon, (:InstantRate, :InstantYield), (:Day)) ]
+                      (:ConstantMaturity, (:Nominal, :Real), (:Daily, :Weekly, :Monthly)),
+                      (:Forward, (:InstantRate, :TermPremium), (:Daily,)),
+                      (:BreakevenInflation, (:InstantRate, :TermPremium), (:Daily, :Monthly)),
+                      (:ZeroCoupon, (:InstantRate, :InstantYield), (:Daily)) ]
 
 """
     TreasuryRates 
-       (:ConstantMaturity   ==> (:Nominal, :Real) => (:Day, :Month, :Year) -> (Day(n), Month(n)),
+       (:ConstantMaturity   ==> (:Nominal, :Real) => (:Daily, :Weekly, :Monthly) -> (Day(n), Month(n)),
        (:Forward            ==> ()), 
-       (:BreakevenInflation ==> (:InstantRate, :TermPremium)  => (:Day, :Month) : (Day(n), Month(n), Year(n)), 
-       (:ZeroCoupon         ==> (:InstantRate, :InstantYield) => (:Day))  
+       (:BreakevenInflation ==> (:InstantRate, :TermPremium)  => (:Daily, :Monthly) : (Day(n), Month(n), Year(n)), 
+       (:ZeroCoupon         ==> (:InstantRate, :InstantYield) => (:Daily))  
     IndexSeries    
-        (:TradeWeighted  => (:Day, :Week, :Month))
+        (:TradeWeighted  => (:Daily, :Weekly, :Monthly))
     SpotSeries    
         (:Gold => (:Morning[:USD,:EURO], :Afternoon[:USD.:EURO]) ),
         (:CrudeOil => (:WestTexas[:USD], :Brent[:USD]) ),
@@ -23,76 +23,80 @@ const FredKeyTerms = [(:Nominal, :Real, :Spot),
 const FredTreasuryRates = Dict([
   :ConstantMaturity => Dict([
     :Nominal => Dict([
-              :Day => Dict([
+              :Daily => Dict([
                        Year(30) => "DGS30", Year(20) => "DGS20",  Year(10) => "DGS10", Year(7) => "DGS7", 
                        Year(5) => "DGS5",  Year(3) => "DGS3", Year(2) => "DGS2",  Year(1) => "DGS1", 
                        Month(6) => "DGS6MO", Month(3) => "DGS3MO", Month(1) => "DGS1MO",  
                        Day(1) => "DGS1DA",
-                     ]),
-              :Week => Dict([
+              ]), # :Daily
+              :Weekly => Dict([
                        Year(30) => "WGS30", Year(20) => "WGS20",  Year(10) => "WGS10", Year(7) => "WGS7", 
                        Year(5) => "WGS5",  Year(3) => "WGS3", Year(2) => "WGS2",  Year(1) => "WGS1", 
                        Month(6) => "WGS6MO", Month(3) => "WGS3MO", Month(1) => "WGS1MO",  
                        Day(1) => "WGS1DA",
-                     ]),
-              :Month => Dict([
+              ]), # :Weekly
+              :Monthly => Dict([
                        Year(30) => "MGS30", Year(20) => "MGS20",  Year(10) => "MGS10", Year(7) => "MGS7", 
                        Year(5) => "MGS5",  Year(3) => "MGS3", Year(2) => "MGS2",  Year(1) => "WGS1", 
                        Month(6) => "MGS6MO", Month(3) => "MGS3MO", Month(1) => "MGS1MO",  
                        Day(1) => "MGS1DA",
-                    ]),
+              ]), # :Monthly
           ]),   # :Nominal       
    :Real =>  Dict([
-              :Day => Dict([ 
+              :Daily => Dict([ 
                        Year(30) => "DFII30", Year(20) => "DFII20",  Year(10) => "DFII10",
                        Year(7) => "DFII7",  Year(5) => "DFII5",
-                     ]),
-              :Week => Dict([
+              ]), # :Daily
+              :Weekly => Dict([
                       Year(30) => "WFII30", Year(20) => "WFII20",  Year(10) => "WFII10",
                       Year(7) => "WFII7",  Year(5) => "WFII5",
-                    ]),
-              :Month => Dict([
+              ]), # :Weekly
+              :Monthly => Dict([
                       Year(30) => "NFII30", Year(20) => "MFII20",  Year(10) => "MFII10",
                       Year(7) => "MFII7",  Year(5) => "MFII5",
-                    ]),
+              ]), # :Monthly
           ]),  # :Real
   ]),  # ConstantMaturity
 
    :BreakevenInflation => Dict([
-             :Day => Dict([
-               Year(30) => "T30YIEM", Year(20) => "T20YIEM",  Year(10) => "T10YIEM",  Year(5) => "T5YIEM" ]),
-             :Month => Dict([
-               Year(30) => "T30YIEM", Year(20) => "T20YIEM",  Year(10) => "T10YIEM", Year(5) => "T5YIEM" ]),
+             :Daily => Dict([
+               Year(30) => "T30YIEM", Year(20) => "T20YIEM",  Year(10) => "T10YIEM",  Year(5) => "T5YIEM"
+             ]), # :Daily
+             :Monthly => Dict([
+               Year(30) => "T30YIEM", Year(20) => "T20YIEM",  Year(10) => "T10YIEM", Year(5) => "T5YIEM" 
+             ]), # :Monthly
            ]),   # :BreakevenInflationRate
         
    :Forward => Dict([
         :InstantRate => Dict([
-               :Day => Dict([
+               :Daily => Dict([
                       Year(10) => "THREEFF10", Year(9) => "THREEFF9",  Year(8) => "THREEFF8",  Year(7) => "THREEFF7",
                       Year(6) => "THREEFF6",  Year(5) => "THREEFF5",  Year(4) => "THREEFF4",  Year(3) => "THREEFF3",
-                      Year(2) => "THREEFF2", Year(1) => "THREEFF1" ]),
-             ]),
+                      Year(2) => "THREEFF2", Year(1) => "THREEFF1" 
+                ]), # :Daily
+             ]), # :InstantRate
        :TermPremium => Dict([
-               :Day => Dict([
+               :Daily => Dict([
                       Year(10) => "THREEFFTP10", Year(9) => "THREEFFTP9",  Year(8) => "THREEFFTP8",  Year(7) => "THREEFFTP7",
                       Year(6) => "THREEFFTP6",  Year(5) => "THREEFFTP5",  Year(4) => "THREEFFTP4",  Year(3) => "THREEFFTP3",
-                      Year(2) => "THREEFFTP2", Year(1) => "THREEFFTP1" ]),
-             ]),
+                      Year(2) => "THREEFFTP2", Year(1) => "THREEFFTP1" 
+                ]), # :Daily
+             ]), # :TermPremium
      ]),         # :Forward
 
   :ZeroCoupon => Dict([
         :InstantYield => Dict([
-               :Day => Dict([
+               :Daily => Dict([
                       Year(10) => "THREEFY10", Year(9) => "THREEFY9",  Year(8) => "THREEFY8",  Year(7) => "THREEFY7",
                       Year(6) => "THREEFY6",  Year(5) => "THREEFY5",  Year(4) => "THREEFY4",  Year(3) => "THREEFY3",
                       Year(2) => "THREEFY2", Year(1) => "THREEFY1" ]),
-             ]),
+               ]), # :Daily
        :TermPremium => Dict([
-               :Day => Dict([
+               :Daily => Dict([
                       Year(10) => "THREEFYTP10", Year(9) => "THREEFYTP9",  Year(8) => "THREEFYTP8",  Year(7) => "THREEFYTP7",
                       Year(6) => "THREEFYTP6",  Year(5) => "THREEFYTP5",  Year(4) => "THREEFYTP4",  Year(3) => "THREEFYTP3",
                       Year(2) => "THREEFYTP2", Year(1) => "THREEFYTP1" ]),
-             ]),
+               ]), # :Daily
      ]),           # :ZeroCoupon
      
   ]); # FredTreasuryRates
@@ -100,42 +104,50 @@ const FredTreasuryRates = Dict([
 const FredIndexedSeries = Dict([
     
        :TradeWeightedUSD => Dict([
-          :Day => Dict([
+          :Daily => Dict([
             :Major => "DTWEXM", :Broad => "DTWEXB" 
-          ]), # :Day
-          :Week => Dict([
+          ]), # :Daily
+          :Weekly => Dict([
             :Major => "TWEXM", :Broad => "TWEXMB" 
-           ]), # :Week
-          :Month => Dict([
+           ]), # :Weekly
+          :Monthly => Dict([
              :Nominal => Dict([ :Major => "TWEXMMTH", :Broad => "TWEXBMTH" ]),
              :Real    => Dict([ :Major => "TWEXMPA",  :Broad => "TWEXBPA"  ]),
-             ]), # :Month
-           ]),    # :TradeWeightedUSD
+           ]), # :Monthly
+       ]),    # :TradeWeightedUSD
   
   ]); # FredIndexedSeries
 
 const FredSpotSeries = Dict([
     
-  :Gold => Dict([ 
+  :Gold => Dict([
+    :Daily => Dict([   
       :Morning => Dict([
                   :USD  => Dict([ Day(1) => "GOLDAMGBD228NLBM" ]),
                   :Euro => Dict([ Day(1) => "GOLDAMGBD230NLBM" ]), 
-        ]), # :Morning
+      ]), # :Morning
       :Afternoon => Dict([
                   :USD  => Dict([ Day(1) => "GOLDPMGBD228NLBM" ]),
                   :Euro => Dict([ Day(1) => "GOLDPMGBD230NLBM" ]),
-        ]), # :Afternoon
-   ]), # :Gold
+      ]), # :Afternoon
+    ]), # :Daily      
+  ]), # :Gold
 
- :CrudeOil => Dict([ 
+ :CrudeOil => Dict([
+    :Daily => Dict([    
       :WestTexas => Dict([
-        :USD  => Dict([ Day(1) => "DCOILWTICOM" ]), ]),
+        :USD  => Dict([ Day(1) => "DCOILWTICOM" ]), 
+      ]),
       :Brent => Dict([
-        :USD  => Dict([ Day(1) => "DCOILBRENTEU" ]), ]),
-   ]), # :CrudeOil    
+        :USD  => Dict([ Day(1) => "DCOILBRENTEU" ]), 
+      ]),
+    ]), # :Daily         
+ ]), # :CrudeOil    
 
  :NaturalGas => Dict([
+    :Daily([    
         :USD  => Dict([ Day(1) => "DHHNGSP" ]),
-   ]), # :NaturalGas
+    ]), # :Daily        
+ ]), # :NaturalGas
 
 ]); # FredSpotSeries
