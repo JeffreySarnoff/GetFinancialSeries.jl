18c60fb742f1b16f8006ae7becfa275be340c3b4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-06-10 04:49:24 -0400

    Create authorization.jl

--- src/truefx/authorization.jl
+++ src/truefx/authorization.jl
@@ -3,6 +3,8 @@ TrueFX = Val{:TrueFX}
 TrueFX_username = "TrueFX_username"
 TrueFX_password = "TrueFX_password"
 TrueFX_qualifier = "eurates"
+TrueFX_format = "csv" # or "html"
+TrueFX_currencypairs = "EUR/USD,GBP/USD,CAN/USD,AUD/USD"
 ENV["TrueFX_sessionid"] = ""
 
 try
@@ -19,9 +21,25 @@ end
 
 const truefx_connect_url = "http://webrates.truefx.com/rates/connect.html"
 const truefx_authentication_url = string(truefx_connect_url,"?u=",TrueFX_username,"&p=",TrueFX_password)
-const truefx_authorization_url = string(truefx_authentication_url,"&q=",TrueFX_qualifier)
+truefx_authorization_url = string(truefx_authentication_url,"&q=",TrueFX_qualifier,"&f=",TrueFX_format)
 
+# e.g. "EUR/USD" or "EUR/USD,EUR/GBP"
+function truefx_currency_pairs(currencypairs::V) where V<:AbstractVector{S} where S<:String
+   return join(V, ",")
+end
+truefx_currency_pairs(currencypairs::String) = currencypairs
+
+function truefx_currency_pairs!(currencypairs::String)
+    global truefx_authorization_url
+    truefx_authorization_url = split(truefx_authorization_url,"&c=")[1]
+    truefx_authorization_url = string(truefx_authorization_url,"&c=",currencypairs)
+    return nothing
+end   
+
+truefx_authorization_url = truefx_currency_pairs!(truefx_currency_pairs)
+   
 function sessionid(::Type{TrueFX}; force::Bool=false)
+    global truefx_authorization_url
     if force || length(ENV["TrueFX_sessionid"]) == 0
         response = Requests.get(truefx_authorization_url)
         newsessionid = chomp(readstring(response))
